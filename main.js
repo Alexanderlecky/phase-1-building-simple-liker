cc1057df7aad858b36e7c501237d37e51e1e5286
Alexander Karanja <alexanderkaranja78@gmail.com>
2024-07-10 22:23:28 +0300

    building simple likes

--- main.js
+++ main.js
@@ -3,7 +3,33 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  const errorModal = document.getElementById("model");
+  errorModal.classList.add("hidden");
 
+  const hearts = document.querySelectorAll(".like-glyph");
+  hearts.forEach(heart => {
+    heart.addEventListener("click", () => {
+      if (heart.innerText === "♡") {
+        mimicServerCall()
+        .then(() => {
+          heart.innerText = "♥";
+          heart.classList.add("activated-heart");
+        })
+        .catch(error => {
+          errorModal.classList.remove("hidden");
+          document.getElementById("model-message").innerText = error;
+          setTimeout(() => {
+            errorModel.classList.add("hidden");
+          }, 3000);
+        });
+      }else {
+        heart.innerText = "♡"
+        heart.classList.remove("activated-heart");
+      }
+    });
+  });
+});
 
 
 
